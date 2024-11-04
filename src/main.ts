8121a25490204a2523c4d4a6205e72e9bd7e05f3
Mykola Maik <mykoluamaik@gmail.com>
2024-11-04 20:20:32 +0200

    add env port variable

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const PORT = process.env.PORT || 4000;
 
   app.enableCors();
 
-  await app.listen(4000);
+  await app.listen(PORT, '0.0.0.0');
 }
 
 bootstrap();
